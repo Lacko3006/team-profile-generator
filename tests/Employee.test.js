95d0603e6afdb947de82645407d1733b99b7133b
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-12-01 18:39:18 +0000

    added test to class (not working) ''

---
+++ tests/Employee.test.js
@@ -0,0 +1,13 @@
+const Employee = require('../lib/employee');
+const employeeTest = require('../lib/employee');
+
+describe("Employee Name"), () => {
+ const employee = new Employee("Sam")
+
+
+describe ('Employee Class', () => {
+    it('should pass if it is returned', () => {
+        expect(Employee.getNAme()).toBe("Sam")
+    });
+});
+}

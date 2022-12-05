1e1e7667ae5cc6ab75f5992f25904f4550681839
Sam Laxton <samlaxton@sams-air-2.home>
2022-12-05 16:13:04 +0000

    Employee Test finished

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,13 +1,29 @@
-const Employee = require('../lib/employee');
-const employeeTest = require('../lib/employee');
+const Employee = require("../lib/Employee");
 
-describe("Employee Name"), () => {
- const employee = new Employee("Sam")
+describe("Employee Class", () => {
+  const employee = new Employee("Sam", 13, "lacko_6789@hotmail.co.uk");
 
-
-describe ('Employee Class', () => {
-    it('should pass if it is returned', () => {
-        expect(Employee.getNAme()).toBe("Sam")
+  describe("getName function", () => {
+    it("employee name returned", () => {
+      expect(employee.getName()).toBe("Sam");
     });
+  });
+
+  describe("getId function", () => {
+    it("ID returned", () => {
+      expect(employee.getId()).toBe(13);
+    });
+  });
+
+  describe("getEmail function", () => {
+    it("Email returned", () => {
+      expect(employee.getEmail()).toBe("lacko_6789@hotmail.co.uk");
+    });
+  });
+
+  describe("getRole function", () => {
+    it("Role returned", () => {
+      expect(employee.getRole()).toBe("Employee");
+    });
+  });
 });
-}
